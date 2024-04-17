b3be02699bb3495a404669979bf2d1255b332c38
Adrian <adrian.hamzah15@gmail.com>
2024-04-17 20:05:45 +0800

    feat: edit color styling

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,14 @@ import Typewriter from 'typewriter-effect';
 
 export default function Home() {
    return (
-    <div className='min-h-screen bg-black flex justify-center items-center'> 
-      <div className='text-white text-center text-4xl font-bold'> 
+    <div className='min-h-screen bg-gray-900 flex justify-center items-center'> 
+      <div className='text-stone-300 text-center text-4xl font-bold'> 
         <h1>
           <Typewriter
             options={{
-              autoStart: true,
               loop: true,
-              delay: 80,
+              autoStart: true,
+              delay: 100,
               strings: ["AFK-3's Frontend"]
             }}
           />
